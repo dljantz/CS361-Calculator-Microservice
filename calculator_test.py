357ef954351809188334827f451a377e5f6bc06f
David Jantz <dljantz@gmail.com>
2026-05-12 14:52:43 -0500

    Added explanation of test file

--- calculator_test.py
+++ calculator_test.py
@@ -1,3 +1,15 @@
+"""
+If you don't feel like running this, just paste this into your browser's address bar to test:
+https://cs361-calculator-microservice.onrender.com/calculate?m1=5&m2=10&d1=4&d2=2
+Format the output of your monolith to mirror the structure of the url above.
+You must have either:
+-- m1 and m2 (for multiplication)
+-- d1 and d2 (for division)
+-- m1, d1, and d2 (for combined)
+Any other combination of parameters will return an error message.
+You can also test division by zero by setting d2 to 0, which should also return an error message.
+"""
+
 import requests
 URL = "https://cs361-calculator-microservice.onrender.com/calculate"
 
